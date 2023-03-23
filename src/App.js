1b1e40dfdd897927895c5220e0cea4904a468dfa
Your Name~ <you@example.com>
2023-03-22 21:21:56 -0300

    Começando a criar a estrutura dos cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
+import Posicao from './componentes/posicao';
 
 function App() {
+
+  const posicoes = [
+    {
+      nome: '',
+      corPrimaria: '',
+      corSecundaria:''
+    },
+    {
+      nome: '',
+      corPrimaria '',
+      corSecundaria:''
+    },
+    {
+      nome: '',
+      corPrimaria: '',
+      corSecundaria:''
+    },
+    {
+      nome: '',
+      corPrimaria: '',
+      corSecundaria:''
+    },
+  ]
+  const [colaboradores, setColaboradores] = useState ([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) =>{
+    console.log(colaborador)
+    setColaboradores({...colaboradores, colaborador})
+
+
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario></Formulario>
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)  } />
+      <Posicao nome ="Levantes"/>
+      <Posicao nome ="Líberos"/>
+      <Posicao nome ="Ponteiros"/>
+      <Posicao nome ="Coringas"/>
     </div>
   );
 }
